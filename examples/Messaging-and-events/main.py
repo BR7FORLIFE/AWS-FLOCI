b1d615522fc661b0f9f09498756e65648761167c
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-08-13 09:39:19 -0500

    feat: Enhance SQS and SNS process handling and update dependencies

--- examples/Messaging-and-events/main.py
+++ examples/Messaging-and-events/main.py
@@ -4,9 +4,15 @@ from consumer import ConsumerSQSPrintMessage
 
 
 if __name__ == "__main__":
+    # SQS Process
     producer = Process(target= ProducerSQSSendMessage)
     consumer = Process(target=ConsumerSQSPrintMessage)
 
-    # comenzamos a crear los procesos
+    # comenzamos a crear los procesos de SQS
     producer.start()
     consumer.start()
+
+    # SNS Process
+    
+
+    # comenzamos a crear los procesos de SNS
